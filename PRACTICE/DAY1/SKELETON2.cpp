0996ef2cd37a4567d31ce83e9edf8976ecb9fc97
ISHAN142005 <ishanbohra1414@gmail.com>
2025-12-26 13:53:22 +0530

    Day1practice

---
+++ PRACTICE/DAY1/SKELETON2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+
+    cout<<"its christmas today";
+
+    //single line comment
+/*
+
+multiple 
+line 
+comment
+*/
+    return 0;
+}
---
+++ PRACTICE/DAY1/variable.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=1,b=4;
+    int sum=a+b;
+    cout<<sum<<a;
+
+    return 0;
+}
