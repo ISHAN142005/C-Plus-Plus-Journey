ad95af571d56e53897f07d3ff15baca7cf36f686
ISHAN142005 <ishanbohra1414@gmail.com>
2025-12-27 21:47:33 +0530

    hrque

---
+++ PRACTICE/DAY3/d3a2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int max_four(int a,int b,int c,int d);
+
+int max_four(int a,int b,int c,int d){
+    int max=a;
+    
+    if(b>max){
+        max=b;
+    }
+    if(c>max){
+        max=c;
+    }
+    if(d>max){
+        max=d;
+    }
+    return max;
+}
+int main(){
+    int a,b,c,d;
+    cin>>a>>b>>c>>d;
+    cout<<max_four(a,b,c,d)<<endl;
+    return 0;
+}
