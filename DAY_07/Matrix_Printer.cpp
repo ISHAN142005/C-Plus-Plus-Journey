4fe5a732a316fbd9ce6751e69ed35e4fab6dd59e
ISHAN142005 <ishanbohra1414@gmail.com>
2025-12-31 15:32:13 +0530

    MatrixPrinter

---
+++ DAY_07/Matrix_Printer.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int matrix[3][3];
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << "enter value matrix element [" << i + 1 << "][" << j + 1 << "]:";
+            cin >> matrix[i][j];
+            cout << endl;
+        }
+    }
+    cout << "The matix is:" << endl;
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
