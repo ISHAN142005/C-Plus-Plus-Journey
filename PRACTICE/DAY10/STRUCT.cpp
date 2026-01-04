a8dff3d839e3652167388643d486fc1ec673c1cf
ISHAN142005 <ishanbohra1414@gmail.com>
2026-01-04 21:11:21 +0530

    Structure1

---
+++ PRACTICE/DAY10/STRUCT.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+typedef struct Students{
+    string Name;
+    string RollNo;
+    int Age;
+}S;
+
+
+int main(){
+
+S Student1;
+cout<<"Enter student Name:";
+cin>>Student1.Name;
+cout<<"Enter student Roll No.:";
+cin>>Student1.RollNo;
+cout<<"Enter student Age:";
+cin>>Student1.Age;
+
+
+
+
+
+
+
+
+    return 0;
+}
