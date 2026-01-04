0fb7e9bdf7a4341954e5b13595a228731d8c18eb
ISHAN142005 <ishanbohra1414@gmail.com>
2026-01-04 21:33:54 +0530

    Enum

---
+++ PRACTICE/DAY10/Enum.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+enum Meal{Breakfast,Lunch,Dinner};
+cout<<Breakfast<<endl;
+cout<<Lunch<<endl;
+cout<<Dinner<<endl;
+
+Meal Ishan=Lunch;
+cout<<Ishan<<endl;
+
+Meal Harry=Dinner;
+cout<<Harry<<endl;
+
+
+
+    return 0;
+}
