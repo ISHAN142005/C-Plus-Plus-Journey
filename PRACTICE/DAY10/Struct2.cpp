92c2bc8ad060f04c8a15128154fde2932cae39b5
ISHAN142005 <ishanbohra1414@gmail.com>
2026-01-04 21:13:11 +0530

    Strcture2

---
+++ PRACTICE/DAY10/Struct2.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+ struct Students{
+    string Name;
+    string RollNo;
+    int Age;
+};
+
+
+int main(){
+
+struct Students Student1;
+cout<<"Enter student Name:";
+cin>>Student1.Name;
+cout<<"Enter student Roll No.:";
+cin>>Student1.RollNo;
+cout<<"Enter student Age:";
+cin>>Student1.Age;
+
+
+
+
+
+
+
+
+    return 0;
+}
